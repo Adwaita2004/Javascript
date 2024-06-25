66247386b2052e55a54499041146a47c73762b09
Adwaita2004 <adwaitakarmakar16062004@gmail.com>
2024-06-25 16:37:40 +0000

    for of loop

---
+++ 05_iterations/for_of_loop.js
@@ -0,0 +1,32 @@
+const arr = [1,2,3,4,5]
+
+for(const num of arr){
+     //console.log(num)
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+ //console.log(`Each char is ${greet}`)
+}
+
+//Map
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+//console.log(map);
+
+for(const [key, value]of map){
+    //console.log(key, ':-', value);
+}
+
+//myObject is not iterable
+
+const programming = ["js", "rb", "cpp", "java", "cpp"]
+
+for(const key in programming){
+    //console.log([key])
+}
+
