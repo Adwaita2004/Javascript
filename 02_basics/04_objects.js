52e433587c10eb5207302634f3e43a5d74502d50
Adwaita2004 <adwaitakarmakar16062004@gmail.com>
2024-06-25 13:15:11 +0000

    Objects2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,6 +22,14 @@ const obj2 = {3: "a", 4: "b"}
 
 //const obj3 = Object.assign({}, obj1, obj2)
 const obj3 = {...obj1, ...obj2}
-console.log(obj3)
+//console.log(obj3)
 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+const{courseInstructor} = course
+console.log(courseInstructor)
 
